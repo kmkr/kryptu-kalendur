c5ea8193d5be9cf8161284291b920076a87407c9
Kris-Mikael Krister <krismikael@protonmail.com>
2016-12-24 00:42:43 +0100

    Improved display on small screens

--- src/luke.js
+++ src/luke.js
@@ -34,9 +34,15 @@ export default class Luke extends PureComponent {
                 <h3>{name}</h3>
 
                 <p>
+                    <div style={{display: 'inline-block'}}>
                     <img className="icon" src="dest/imgs/super_mushroom.png" /> <strong>{winner}</strong>
+                    </div>
+                    <div style={{display: 'inline-block'}}>
                     {bonus && <span>&nbsp;&nbsp;&nbsp;<img className="icon" src="dest/imgs/cherry_bonus.png" /> <strong>{bonus}</strong></span>}
+                    </div>
+                    <div style={{display: 'inline-block'}}>
                     {extra.name && <span>&nbsp;&nbsp;&nbsp;<img className="icon" src="dest/imgs/carrot_bonus.png" /> <strong>{extra.name}</strong>(+{extra.points})</span>}
+                    </div>
                 </p>
 
             </div>
